bef1e2ac37140f52742c3e4d84b88ea90c9996c9
tougshire <tougshire@tougshire.com>
2024-09-09 06:33:16 -0400

    Initial commit

---
+++ models.py
@@ -0,0 +1,190 @@
+import datetime
+from django import forms
+from django.db import models
+
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase
+
+from wagtail.models import Page, Orderable
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel, InlinePanel, MultiFieldPanel
+from wagtail.search import index
+from wagtail.snippets.models import register_snippet
+from wagtail.contrib.settings.models import (
+    BaseGenericSetting,
+    BaseSiteSetting,
+    register_setting,
+)
+
+
+class BlogIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro')
+    ]
+
+    def get_context(self, request):
+        context = super().get_context(request)
+        BlogPages = self.get_children().live().order_by('-first_published_at')
+        context['BlogPages'] = BlogPages
+        return context
+
+@register_snippet
+class BlogPageTag(TaggedItemBase):
+    content_object = ParentalKey(
+        'BlogPage',
+        related_name='tagged_items',
+        on_delete=models.CASCADE
+    )
+    hide_from_lists = models.BooleanField('hide from lists', default=False, help_text="if this tag should be hidden from the list of tags in an BlogPage's meta section and similar locations")
+
+class BlogPage(Page):
+    date = models.DateField("Post date", default=datetime.date.today)
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+    authors = ParentalManyToManyField('pyviivvii.Author', blank=True)
+    tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
+
+    parent_page_types = ['BlogIndexPage']
+
+    def get_context(self, request):
+        context=super().get_context(request)
+        context['visible_tags']=[]
+        for tag in context['page'].tags.all():
+            if not tag.name[0] == '_':
+                context['visible_tags'].append(tag.name)
+
+        return context
+
+
+    def main_image(self):
+        gallery_item = self.gallery_images.first()
+        if gallery_item:
+            return gallery_item.image
+        else:
+            return None
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        MultiFieldPanel(
+            [
+                FieldPanel('date'),
+                FieldPanel('authors', widget=forms.CheckboxSelectMultiple),
+                FieldPanel('tags'),
+            ],
+            heading="Blog information"
+        ),
+        FieldPanel('intro'),
+        FieldPanel('body'),
+        InlinePanel('gallery_images', label="Gallery images"),
+    ]
+
+class BlogPageGalleryImage(Orderable):
+    page = ParentalKey(BlogPage, on_delete=models.CASCADE, related_name='gallery_images')
+    image = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
+    )
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = [
+        FieldPanel('image'),
+        FieldPanel('caption'),
+    ]
+
+@register_snippet
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    author_image = models.ForeignKey(
+        'wagtailimages.Image', null=True, blank=True,
+        on_delete=models.SET_NULL, related_name='+'
+    )
+
+    panels = [
+        FieldPanel('name'),
+        FieldPanel('author_image'),
+    ]
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Authors'
+
+class BlogTagIndexPage(Page):
+
+    def get_context(self, request):
+
+        tag = request.GET.get('tag')
+        BlogPages = BlogPage.objects.filter(tags__name=tag)
+
+        context = super().get_context(request)
+        context['BlogPages'] = BlogPages
+        return context
+
+class BlogTagsIndexPage(Page):
+
+    included_tag_names_string = models.CharField("tags included", max_length=255, blank=True, help_text="A comma separated list of tags to be included in this page")
+    grouped_tag_names_string = models.CharField("tags included", max_length=255, blank=True, help_text="A comma separated list of tags to be used for separating into groups. Post must match included tags to be visible")
+    tag_titles_string = models.CharField("tags titles", max_length=255, blank=True, help_text="A comma separated list of titles to be used instead of the tag names")
+    separate_tag_groups = models.BooleanField(default=True, help_text="If the BlogPages should be separated by tag")
+    repeat_BlogPages = models.BooleanField(default=True, help_text="If separated by tag, if BlogPages that have multiple included tags should be repeated")
+    show_tag_titles = models.BooleanField(default=True, help_text='If the tag name should be displayed as a title to accompany the BlogPages')
+
+    content_panels = Page.content_panels + [
+        FieldPanel('included_tag_names_string'),
+        MultiFieldPanel(
+            [
+                FieldPanel('tag_titles_string'),
+                FieldPanel('separate_tag_groups'),
+                FieldPanel('repeat_BlogPages'),
+                FieldPanel('show_tag_titles')
+            ]
+        )
+    ]
+
+    def get_context(self, request):
+
+        included_blog_tag_groups = []
+
+        included_tag_names = self.included_tag_names_string.split(',')
+        tag_titles = self.tag_titles_string.split(',')
+
+        used_tag_names=[]
+
+        for i in range(len(included_tag_names)):
+            included_tag_name = included_tag_names[i].strip()
+            tag_title = tag_titles[i].strip() if len(tag_titles ) > i else included_tag_name
+            used_tag_names.append(included_tag_name)
+            included_blog_tag_group={}
+
+            if not (self.repeat_BlogPages  or self.separate_tag_groups):
+                included_blog_tag_group['pages'] = BlogPage.objects.filter(tags__name=included_tag_name).exclude(tags_name__in=used_tag_names)
+            else:
+                included_blog_tag_group['pages'] = BlogPage.objects.filter(tags__name=included_tag_name)
+
+            if included_blog_tag_group['pages']:
+                included_blog_tag_group['tagname'] = included_tag_name
+                included_blog_tag_group['title'] = tag_title
+                included_blog_tag_groups.append(included_blog_tag_group)
+
+        context = super().get_context(request)
+        context['taggroups'] = included_blog_tag_groups
+        context['show_tag_titles'] = self.show_tag_titles
+
+        return context
+
+@register_setting
+class SiteSpecificImportantPages(BaseSiteSetting):
+    blog_index_page = models.ForeignKey(
+        'wagtailcore.Page', null=True, on_delete=models.SET_NULL, related_name='+'
+    )
+
+    panels = [
+        FieldPanel('blog_index_page'),
+    ]
---
+++ wagtail_hooks.py
@@ -0,0 +1,13 @@
+from wagtail_modeladmin.options import ModelAdmin, modeladmin_register
+from .models import BlogPage
+
+
+class BlogPageAdmin(ModelAdmin):
+    model = BlogPage
+    menu_icon = 'pilcrow'  # change as required
+    menu_order = 200  # will put in 3rd place (000 being 1st, 100 2nd)
+    add_to_settings_menu = False  # or True to add your model to the Settings sub-menu
+    exclude_from_explorer = False # or True to exclude pages of this type from Wagtail's explorer view
+
+# Now you just need to register your customised ModelAdmin class with Wagtail
+modeladmin_register(BlogPageAdmin)
