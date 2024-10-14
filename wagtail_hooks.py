e497dc9c01fe2cd6f275e1d38e3c214f4c838a8c
tougshire <tougshire@tougshire.com>
2024-10-14 08:39:27 -0400

    Create icon for article

--- wagtail_hooks.py
+++ wagtail_hooks.py
@@ -2,13 +2,16 @@ from wagtail_modeladmin.options import ModelAdmin, modeladmin_register, hooks
 from wagtail.admin.viewsets.pages import PageListingViewSet
 from .models import ArticlePage, SidebarArticlePage
 
+@hooks.register("register_icons")
+def register_icons(icons):
+    return icons + ['wibekwa/article.svg']
+
 class ArticlePageListingViewSet(PageListingViewSet):
-    icon = "globe"
+    icon = "article"
     menu_order = 100  # will put in 3rd place (000 being 1st, 100 2nd)
     menu_label = "Articles"
     add_to_admin_menu = True
     model = ArticlePage
-
 
 article_page_listing_viewset = ArticlePageListingViewSet("article_pages")
 @hooks.register("register_admin_viewset")
@@ -16,7 +19,7 @@ def register_article_page_listing_viewset():
     return article_page_listing_viewset
 
 class SidebarArticlePageListingViewSet(PageListingViewSet):
-    icon = "globe"
+    icon = "article"
     menu_order = 110  # will put in 3rd place (000 being 1st, 100 2nd)
     menu_label = "Sidebar Articles"
     add_to_admin_menu = True
